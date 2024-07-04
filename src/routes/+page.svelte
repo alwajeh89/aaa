ef7a4bb836e7d160e3c78cabeaab306cfeab0ee0
AAA <alwajeh89@gmail.com>
2024-07-04 23:21:55 +0300

    centered white rectangle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let x = 5;
+ function dec (){
+
+    x/=2;
+
+ }
+ function dec1 (){
+
+x*=2;
+}
+</script>
+<svelte:head>
+    <title>
+        kkkk
+    </title>
+</svelte:head>
+<div class=" ">
+    <div class="bg-white h-[40dvh] w-[40dvw] rounded-3xl">
+<h5 class="text-2xl text-purple-300">  Welcome {x}</h5>
+<hr>
+</div>
+</div>
